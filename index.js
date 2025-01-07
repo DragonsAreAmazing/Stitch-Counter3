ccf7d9533dd5b145e4c6cdc52ae421ab5b491af1
DragonsAreAmazing <131500190+DragonsAreAmazing@users.noreply.github.com>
2025-01-07 17:21:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
+let count2 = 0
 
 function increment() {
     count += 5
-    countEl.textContent = count
+    countEl.textContent = count = count2
+}
+
+function increment290 {
+    count2++
+    countEl.textContent = count + count2
 }
 
 function save() {
